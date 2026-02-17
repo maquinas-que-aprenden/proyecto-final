1e2e77ce264337affb479b68ab127c96f60f1339
Natalia Garea García <natgarea@gmail.com>
2026-02-17 15:58:40 +0100

    Actualizar terraform

--- infra/terraform/ansible.tf
+++ infra/terraform/ansible.tf
@@ -2,7 +2,7 @@
 resource "local_file" "ansible_inventory" {
   content  = <<-EOT
     [normabot]
-    ${aws_instance.normabot_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/tu-llave.pem
+    ${aws_instance.normabot_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/aws.pem
   EOT
   filename = "../ansible/inventory.ini"
 }
