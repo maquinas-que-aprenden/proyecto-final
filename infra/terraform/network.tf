3c983248c49b496dee98c9ff3549249297f3d453
Natalia Garea García <natgarea@gmail.com>
2026-02-18 11:59:46 +0100

    Añadir https y autenticación básica a MLflow

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -10,18 +10,18 @@ resource "aws_security_group" "normabot_sg" {
     cidr_blocks = ["0.0.0.0/0"] 
   }
 
-  # Streamlit UI default
+  # HTTP (redirige a HTTPS via nginx)
   ingress {
-    from_port   = 8501
-    to_port     = 8501
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # MLflow
+  # HTTPS (nginx)
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
