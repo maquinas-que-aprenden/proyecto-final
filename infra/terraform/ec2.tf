c4b2dde182c26ddf638428c7ec3d9a1627d9b34b
Natalia Garea García <natgarea@gmail.com>
2026-02-20 11:11:11 +0100

    Actualizar para separar SGs y añadir roles para ec2 de bedrock

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -1,22 +1,44 @@
 resource "aws_instance" "normabot_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  
+  ami                         = var.ami_id
+  instance_type               = "t3.micro"
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.normabot_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile = aws_iam_instance_profile.normabot_agent_profile.name
 
-  # EBS 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 8
     volume_type           = "gp3"
-    delete_on_termination = false # para no perder datos
-    
-    tags = {
-      Name = "NormaBot-Disk"
-    }
+    delete_on_termination = true
   }
+
+  tags = {
+    Name = "NormaBot-Server"
+  }
+}
+
+resource "aws_ebs_volume" "normabot_data" {
+  availability_zone = "eu-west-1a"
+  size              = 8
+  type              = "gp3"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "NormaBot-Disk"
+  }
+}
+
+resource "aws_volume_attachment" "normabot_data_attach" {
+  device_name  = "/dev/sdf"
+  volume_id    = aws_ebs_volume.normabot_data.id
+  instance_id  = aws_instance.normabot_server.id
+  force_detach = true
+  skip_destroy = true
 }
 
 resource "aws_instance" "mlflow_server" {
@@ -25,7 +47,7 @@ resource "aws_instance" "mlflow_server" {
   key_name      = var.key_name
 
   subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.normabot_sg.id]
+  vpc_security_group_ids = [aws_security_group.mlflow_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
